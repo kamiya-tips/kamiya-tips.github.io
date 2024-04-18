e7cbbbe7337885e31919de6042d6c6ac6282fafe
kamiya <kamiya>
2024-04-18 13:07:18 +0800

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "s6team-HeroTown-0.1.0";
-const contentToCache = [
-    "Build/kamiya-tips.github.io.loader.js",
-    "Build/kamiya-tips.github.io.framework.js.unityweb",
-    "Build/kamiya-tips.github.io.data.unityweb",
-    "Build/kamiya-tips.github.io.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
