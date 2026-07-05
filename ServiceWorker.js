b25fd45a2053a9f7e15fd1fcb2c02bb4da383811
kamiya <kamiya>
2026-07-05 21:11:51 +0800

    add power

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "S6Team-Dnd4e-1.0";
 const contentToCache = [
-    "Build/c673b2a2badcfddade5b63d934dec087.loader.js",
+    "Build/397586e622cb7b3d714483f3cd2df211.loader.js",
     "Build/66061c6da569ddeec3c93dff6cd9ea63.framework.js.unityweb",
-    "Build/2cf80b07bf49bb11896617bd8c319d5f.data.unityweb",
-    "Build/aad97bfbbf1bd53c6ca4b11e16542f13.wasm.unityweb",
+    "Build/54a74fe29933951c080bc14918842bbc.data.unityweb",
+    "Build/e2cf61f343b5c167e9ad1dab1e87b687.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
